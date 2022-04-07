23bc4ffbeb6ae917d74a142941b26411950387cf
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-04-07 22:57:27 +0200

    String mutations

---
+++ ex09/src/index.js
@@ -0,0 +1,24 @@
+// Only change code below this line
+function myMutation(arr) {
+return arr[1].toLowerCase()
+.split('')
+.every(function(letter) {
+  return arr[0].toLowerCase()
+    .indexOf(letter) != -1;
+});
+}
+// Only change code above this line
+console.log(myMutation(["hello", "hey"])); // Change this line
+console.log(myMutation(["hello", "Hello"])); // Change this line
+console.log(myMutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"])); // Change this line
+console.log(myMutation(["Mary", "Army"])); // Change this line
+console.log(myMutation(["Mary", "Aarmy"])); // Change this line
+console.log(myMutation(["Alien", "line"])); // Change this line
+console.log(myMutation(["floor", "for"])); // Change this line
+console.log(myMutation(["hello", "neo"])); // Change this line
+console.log(myMutation(["voodoo", "no"])); // Change this line
+console.log(myMutation(["ate", "date"])); // Change this line
+console.log(myMutation(["Tiger", "Zebra"])); // Change this line
+console.log(myMutation(["Noel", "Ole"])); // Change this line
+
+module.exports = myMutation;
