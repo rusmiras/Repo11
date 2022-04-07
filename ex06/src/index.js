b9efc404b3f5b71f3201009d772dd1ca4e0cdfee
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-04-07 14:32:03 +0200

    Nested arrays

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+// Only change code below this line
+var myNestedArray = [["KitKat", 5]];
+function myNestedFunction(arr) {
+    myNestedArray = arr;
+    return myNestedArray;
+  }
+// Only change code above this line
+console.log(myNestedFunction([["Toblerone", 5], ["Milka", 3]])); // Change this line
+module.exports = myNestedFunction;
